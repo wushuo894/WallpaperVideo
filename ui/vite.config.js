8f1573367139562b457ac35ae0d013570b66b82d
wushuo <wushuo894@outlook.com>
2024-02-06 22:35:40 +0800

    add iml

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8080',
+                target: 'http://192.168.5.3:8080',
                 changeOrigin: true,
                 // rewrite: (path) => path.replace(/^\/api/, ''),
             }
